6e980feb3ea4c36f8363bdb9dac2de64dce1e31a
Bastian Gruber <foreach@me.com>
2023-05-28 20:31:33 +0200

    Add comments to exolain bytes_to_base64 function better

--- set_1/convert-hex-to-base64/src/lib.rs
+++ set_1/convert-hex-to-base64/src/lib.rs
@@ -23,26 +23,54 @@ fn get_bytes_from_hex() {
     assert_eq!(bytes, b"Hello World");
 }
 
+/// Here's how the conversion works:
+/// 1. Take the binary data and divide it into 8-bit bytes: Our `bytes` input
+/// 2. Group the bytes into sets of three bytes (for a total of 24 bits).
+/// 3. Divide those 24 bits into four sets of six bits.
+/// 4. Convert each set of six bits into a decimal number. This gives you four decimal numbers for each three-byte group.
+/// 5. Map each decimal number to a character in the Base64 alphabet.
 pub fn bytes_to_base64(bytes: &[u8]) -> String {
+    // This is a string containing all possible characters for Base64 encoding.
     let base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    // This is an empty `String` that will store the Base64-encoded result.
     let mut base64 = String::new();
 
+    // This loop goes over the `bytes` slice in chunks of 3 bytes each.
+    // Base64 encoding works on 3 bytes at a time.
     for chunk in bytes.chunks(3) {
+        // This `temp` variable is going to hold the combined value of the bytes in the current chunk.
+        // It's initialized to 0 for each new chunk.
         let mut temp: u32 = 0;
+
+        // This loop goes over each byte in the current chunk.
+        // It shifts the byte to the correct position and adds it to `temp`.
         for (i, &byte) in chunk.iter().enumerate() {
+            // Shift the byte value by (16 - 8 * i) bits and bitwise OR with `temp`.
+            // For the first byte, it will shift by 16 bits, for the second by 8 and for the third by 0.
+            // The bitwise OR operation (|) is used to combine bits together.
             temp |= (byte as u32) << (16 - 8 * i);
         }
 
+        // This loop generates the Base64 characters for the current chunk.
+        // Base64 encoding outputs 4 characters for every 3 bytes.
         for i in 0..4 {
+            // If we have enough bits in the chunk for another Base64 character
             if i * 6 < chunk.len() * 8 {
+                // Extract the next 6 bits from `temp` and use them as an index into `base64_chars`.
                 let index = ((temp >> (18 - 6 * i)) & 63) as usize;
+
+                // Get the Base64 character corresponding to `index` and add it to `base64`.
                 base64.push(base64_chars.chars().nth(index).unwrap());
             } else {
+                // If there aren't enough bits left in `temp` for another Base64 character,
+                // add a padding character to `base64`.
                 base64.push('=');
             }
         }
     }
 
+    // Return the Base64-encoded string.
     base64
 }
 
